04e032ddde35a789188c137440927b212b0ff3b5
benos <benantonn@gmail.com>
2022-10-21 19:33:28 -0500

    Add style transitions, change resolution button

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-for (let i = 0; i < 17; i++) {
-    let rows = document.createElement('div');
-    rows.id = 'row' + i;
-    rows.className = 'border pad row';
-    document.getElementById("container").appendChild(rows);
-}
-
-let x = 0
-for (let i = 0; i < 17; i++) {
-    for (let i = 0; i < 17; i++) {
-        let columns = document.createElement('div');
-        columns.id = 'column';
-        columns.className = 'border pad column';
-        document.getElementById("row" + x).appendChild(columns);
+function canvasCreate(a) {
+    for (let i = 0; i < a; i++) {
+        let rows = document.createElement('div');
+        rows.id = 'row' + i;
+        rows.className = 'border pad row';
+        document.getElementById("container").appendChild(rows);
     }
-    x++;
+
+    let x = 0
+    for (let i = 0; i < a; i++) {
+        for (let i = 0; i < a; i++) {
+            let columns = document.createElement('div');
+            columns.id = 'column';
+            columns.className = 'border pad column';
+            document.getElementById("row" + x).appendChild(columns);
+        }
+        x++;
+    }
+    const divs = document.querySelectorAll('.column');
+    divs.forEach(el => el.addEventListener('mouseover', event => {
+        event.target.classList.add("fill");
+        }));
+        divs.forEach(el => el.addEventListener('transitionend', event => {
+            event.target.classList.remove("fill");
+        }));
 }
+canvasCreate(16)
 
-const divs = document.querySelectorAll('.column');
-
-divs.forEach(el => el.addEventListener('click', event => {
-  event.target.style.backgroundColor = "black";
-  console.log(event.target.getAttribute("data-el"));
-}));
+  res.onclick = () => {
+    const myNode = document.getElementById("container");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.lastChild);
+    }
+    let resNumber = prompt("Enter new resolution")
+    canvasCreate(resNumber);
+    addListeners();
+};
